16db2ab4315227f78ee4f37bf5790c59a959b201
kura495 <104417866+kura495@users.noreply.github.com>
2023-12-04 11:42:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Scenes/State/GamePlayState.cpp
@@ -0,0 +1,51 @@
+#include "Scenes/State/GamePlayState.h"
+
+void GamePlayState::Initialize()
+{
+	//基本機能生成
+	camera_ = new Camera();
+	camera_->Initialize(1280, 720);
+	input = Input::GetInstance();
+	audio = Audio::GetInstance();
+	textureManager_ = TextureManager::GetInstance();
+	light_ = Light::GetInstance();
+
+	DirectX_ = DirectXCommon::GetInstance();
+
+	//
+	//3Dオブジェクト生成
+
+
+}
+
+void GamePlayState::Update()
+{
+#ifdef _DEBUG
+if (input->IspushKey(DIK_LALT)) {
+		camera_->DebugCamera(true);
+}
+else {
+	camera_->DebugCamera(false);
+}
+#endif // _DEBUG
+
+
+
+
+}
+
+void GamePlayState::Draw()
+{
+	//3Dモデル描画ここから
+
+	//3Dモデル描画ここまで	
+
+
+	//Sprite描画ここから
+
+
+	//Sprite描画ここまで
+	
+	//描画ここまで
+}
+
---
+++ Scenes/State/GamePlayState.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "IgameState.h"
+#include "MyEngine.h"
+#include "ModelData.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Mesh.h"
+#include "Sprite.h"
+#include "Light.h"
+#include "Sphere.h"
+#include "TextureManager.h"
+#include "Model.h"
+#define DIRECTINPUT_VERSION 0x0800//DirectInputのバージョン指定
+#include <dinput.h>
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+#include "Base/Camera/Camera.h"
+#include "WorldTransform.h"
+#include "ViewProjection.h"
+#include "Math_Structs.h"
+#include "Base/Math/Quaternion/Quaternion.h"
+
+
+class GamePlayState :public GameState
+{
+public:
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+private:
+	//基本機能ズ
+	MyEngine* myEngine = nullptr;
+	Camera* camera_ = nullptr;
+	TextureManager* textureManager_ = nullptr;	
+	Input* input = nullptr;
+	Audio* audio=nullptr;
+	Light* light_ = nullptr;
+	DirectXCommon* DirectX_ = nullptr;
+	GlobalVariables* globalVariables = nullptr;
+
+
+
+	
+};
