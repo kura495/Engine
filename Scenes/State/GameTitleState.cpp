31ea4416fb650ceae3c1bae4a10d61ab853c768a
kura495 <104417866+kura495@users.noreply.github.com>
2024-01-30 23:10:29 +0900

    add Title

--- Scenes/State/GameTitleState.cpp
+++ Scenes/State/GameTitleState.cpp
@@ -5,6 +5,11 @@ void GameTitleState::Initialize()
 	Editer::GetInstance()->IsEnable(false);
 	input = Input::GetInstance();
 	textureManager_ = TextureManager::GetInstance();
+	textureHundle = textureManager_->LoadTexture("resources/title.png");
+	texture_world_.Initialize();
+
+	texture = std::make_unique<Sprite>();
+	texture->Initialize({ 0.0f,0.0f }, { 0.0f,720.0f }, { 1280.0f,0.0f }, { 1280.0f,720.0f });
 
 	time = 0;
 	IsCanPush = false;
@@ -14,12 +19,18 @@ void GameTitleState::Update()
 {
 	time++;
 	if (time > 60) {
-		StateNo = 1;
+		IsCanPush = true;
 	}
 
+	input->GetJoystickState(joyState);
+	if (joyState.Gamepad.wButtons & XINPUT_GAMEPAD_A) {
+		if (IsCanPush) {
+			StateNo = 1;
+		}
+	}
 }
 
 void GameTitleState::Draw()
 {
-
+	texture->Draw(texture_world_, textureHundle);
 }
