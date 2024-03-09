e9ea20ba793ce99d784b2959b9ec8e367e32ec11
kura495 <104417866+kura495@users.noreply.github.com>
2024-03-09 18:26:04 +0900

    a

--- Base/Math/Matrix/Matrix4x4.cpp
+++ Base/Math/Matrix/Matrix4x4.cpp
@@ -8,6 +8,6 @@ Vector3 Matrix4x4::GetScale(const Matrix4x4& m1) {
 		Vector3::Length({m1.m[0][0],m1.m[0][1],m1.m[0][2]})};
 }
 
-Vector3 Matrix4x4::GetRotation(const Matrix4x4& m1) {
-	return { 0,0,0 };
-}
+//Vector3 Matrix4x4::GetRotation(const Matrix4x4& m1) {
+//	return { 0,0,0 };
+//}
