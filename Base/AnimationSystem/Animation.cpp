8adf40b5213d788ba389a596cdb2b192e74838b6
kura495 <104417866+kura495@users.noreply.github.com>
2024-04-12 14:21:56 +0900

    いらないファイルを削除

--- Base/AnimationSystem/Animation.cpp
+++
@@ -1,8 +0,0 @@
-#include "Animation.h"
-
-//Vector3 Animation::EaseIn(Vector3 Stert, Vector3 End, float Parameter)
-//{
-//	Vector3 result;
-//	result.x = End.x - std::cos((Parameter * (float)std::numbers::pi) / 2);
-//	return 
-//}
