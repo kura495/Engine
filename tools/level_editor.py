732ef104cdcebd9c9a21682482aae77ab1c03627
mikaze <146914469+mikaze0067@users.noreply.github.com>
2024-04-25 11:18:00 +0900

    完了

--- tools/level_editor.py
+++ tools/level_editor.py
@@ -15,7 +15,6 @@ bl_info = {
     "category": "Object"
 }
 
-#オペレータ
 # 頂点を伸ばす
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
 
@@ -46,39 +45,92 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
 
         return {'FINISHED'}
 
-# シーン出力
-class MYADDON_OT_export_scene(bpy.types.Operator):
+#オペレータ シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.WxportHelper):
     bl_idname = "myaddon.myaddon_ot_export_scene"
     bl_label = "シーン出力"
     bl_description = "シーン情報をEXportします"
+    #出力するファイルの拡張子
+    filename_ext = ".scene"
+
+    def export(self):
+        """ファイルに出力"""
+        print("シーン情報出力開始... %r" % self.filepath)
+
+        #ファイルをテキストで形式で書き出し用にオープン
+        #スコープを抜けると自動的にクローズされる
+        with open(self.filepath, "wt")as file:
+
+            #ファイルに文字列を書き込む
+            self.write_and_print("SCENE")
+
+            for object in bpy.context.scene.objects:
+                #親オブジェクトのあるものはスキップ(代わりに親から呼び出すから)
+                if(object.parent):
+                    continue
+                #シーン直下のオブジェクトをルートノード(深さ0)とし、再帰関数で走査
+                self.parse_scene_recursive(file, object, 0)
+
+                """# オブジェクトのタイプと名前を出力
+                self.write_and_print(file,object.type + "-" + object.name)
+                # トランスフォーム情報を出力
+                trans, rot, scale = object.matrix_local.decompose()
+                # QuaertanionからEulerに変換
+                rot = rot.to_euler()
+                # ラジアンから弧度法へ
+                rot.x = math.degrees(rot.x)
+                rot.y = math.degrees(rot.y)
+                rot.z = math.degrees(rot.z)
+                self.write_and_print("Trans(%f,%f,%f)"%(trans.x,trans.y,trans.z) )
+                self.write_and_print("Rot(%f,%f,%f)"%(rot.x,rot.y,rot.z))
+                self.write_and_print("Scale(%f,%f,%f)"%(scale.x,scale.y,scale.z))
+                # 区切りを入れる
+                self.write_and_print("\n")"""
+
 
     def execute(self,context):
         print("シーン情報をExportします")
         # シーン内の全オブジェクトについて
-        for object in bpy.context.scene.objects:
-            # オブジェクトのタイプと名前を出力
-            print(object.type + "-" + object.name)
-            # トランスフォーム情報を出力
-            trans, rot, scale = object.matrix_local.decompose()
-            # QuaertanionからEulerに変換
-            rot = rot.to_euler()
-            # ラジアンから弧度法へ
-            rot.x = math.degrees(rot.x)
-            rot.y = math.degrees(rot.y)
-            rot.z = math.degrees(rot.z)
-            print("Trans(%f,%f,%f)"%(trans.x,trans.y,trans.z))
-            print("Rot(%f,%f,%f)"%(rot.x,rot.y,rot.z))
-            print("Scale(%f,%f,%f)"%(scale.x,scale.y,scale.z))
-            # 親オブジェクトの名前の表示
-            if object.parent:
-                print("Parent:" + object.parent.name)
-            # 区切りを入れる
-            print()
+        
 
+        self.export()
 
-        print("シーン情報をExportしました")
         self.report({'INFO'},"シーン情報をExportしました")
+        print("シーン情報をExportしました")
         return {'FINISHED'}
+
+
+    def write_and_print(self,file,str):
+        print(str)
+
+        file.write(str)
+        file.write('\n')
+
+    def parse_scene_recursive(self, file, object, level):
+        """シーン解析用再帰関数"""
+        #深さ分インデントする
+        indent = ''
+        for i in range(level):
+            indent += "\t"
+
+        #オブジェクト名書き込み
+        self.write_and_print(file,indent + object.type + " - " + object.name)
+        trans,rot,scale = object.matrix_local.decompose()
+        # 回転をQuternionからEuler(3軸での回転角)に変換
+        rat = rat.to_euler()
+        #ラジアンから弧度法に変換
+        rot.x = math.degrees(rot.x)
+        rot.y = math.degrees(rot.y)
+        rot.z = math.degrees(rot.z)
+        #トランスフォーム情報を開示
+        self.write_and_print(file,indent +  "Trans(%f, %f, %f)" % (trans.x,trans.y, trans.z))
+        self.write_and_print(file,indent +  "Rot(%f,%f,%f)"%(rot.x,rot.y,rot.z))
+        self.write_and_print(file,indent +  "Scale(%f,%f,%f)"%(scale.x,scale.y,scale.z))
+        self.write_and_print(file, '')
+
+        #子ノードへ進む(深さが1上がる)
+        for child in object.children:
+            self.parse_scene_recursive(file, child, level + 1)
 
 # トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
