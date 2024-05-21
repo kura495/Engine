b181ee59e0c91ad11c32cb0dde727bccbe8328ac
Taka <k022g0068@g.neec.ac.jp>
2024-05-21 13:05:33 +0900

    dasf

--- Base/Math/Vector/Vector2.h
+++ Base/Math/Vector/Vector2.h
@@ -2,9 +2,119 @@
 struct Vector2 final {
 	float x;
 	float y;
-	Vector2 operator +=(Vector2 other) {
-		this->x = this->x + other.x;
-		this->y = this->y + other.y;
-		return { this->x,this->y};
+
+#pragma region Oparator
+	/*オペレーター*/
+	/*add*/
+	Vector2 operator+(const Vector2& input) {
+		this->x = this->x + input.x;
+		this->y = this->y + input.y;
+		return *this;
 	}
+	Vector2 operator+=(const Vector2& input) {
+
+		*this = *this + input;
+
+		return *this;
+	}
+	/*subtract*/
+	Vector2 operator-(const Vector2& input) {
+		this->x = this->x - input.x;
+		this->y = this->y - input.y;
+		return *this;
+	}
+	Vector2 operator-=(const Vector2& input) {
+
+		*this = *this - input;
+
+		return *this;
+	}
+	/*Multiply*/
+	Vector2 operator*(const Vector2& input) {
+		this->x = this->x * input.x;
+		this->y = this->y * input.y;
+
+		return *this;
+	}
+	Vector2 operator*=(const Vector2& input) {
+
+		*this = *this * input;
+
+		return *this;
+	}
+	/*withfloat*/
+	/*add*/
+	Vector2 operator+(const float& input) {
+
+		return *this;
+	}
+	Vector2 operator+=(const float& input) {
+
+		*this = *this + input;
+
+		return *this;
+	}
+	/*subtract*/
+	Vector2 operator-(const float& input) {
+
+		return *this;
+	}
+	Vector2 operator-=(const float& input) {
+
+		*this = *this - input;
+
+		return *this;
+	}
+	/*Multiply*/
+	Vector2 operator*(const float& input) {
+
+		return *this;
+	}
+	Vector2 operator*=(const Vector2& input) {
+
+		*this = *this * input;
+
+		return *this;
+	}
+#pragma endregion 
 };
+
+// TODO : コピー用
+//#pragma region Oparator
+///*オペレーター*/
+///*add*/
+//Vector2 operator+(const Vector2& input) {
+//
+//	return *this;
+//}
+//Vector2 operator+=(const Vector2& input) {
+//
+//	*this = *this + input;
+//
+//	return *this;
+//}
+///*subtract*/
+//Vector2 operator-(const Vector2& input) {
+//
+//	return *this;
+//}
+//Vector2 operator-=(const Vector2& input) {
+//
+//	*this = *this - input;
+//
+//	return *this;
+//}
+///*Multiply*/
+//Vector2 operator*(const Vector2& input) {
+//	Vector2 result;
+//
+//	return result;
+//}
+//Vector2 operator*=(const Vector2& input) {
+//
+//	*this = *this * input;
+//
+//	return *this;
+//}
+//
+//#pragma endregion 
