70d5d2e372ad4e5e1f5c2d08737d33020222e0c9
k024g6001@g.neec.ac.jp <k024g6001@g.neec.ac.jp>
2024-06-10 22:09:12 +0900

    BoxFilterは完成

--- resources/hlsl/FullScreen.PS.hlsl
+++ resources/hlsl/FullScreen.PS.hlsl
@@ -1,6 +1,5 @@
 #include "FullScreen.hlsli"
 
-
 struct Material
 {
     float32_t3 color; //元の色
@@ -8,6 +7,19 @@ struct Material
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
+};
+static const float32_t2 kIndex3x3[3][3] =
+{
+    { { -1.0f , -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
+    { { -1.0f ,  0.0f }, { 0.0f,  0.0f }, { 1.0f,  0.0f } },
+    { { -1.0f ,  1.0f }, { 0.0f,  1.0f }, { 1.0f,  1.0f } },
+};
+
+static const float32_t kKernel3x3[3][3] =
+{
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
 };
 
 Texture2D<float32_t4> gTexture : register(t0);
@@ -19,9 +31,25 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     output.color = gTexture.Sample(gSampler, input.texcoord);
     
-    if (gMaterial.color.a == 1.0f)
+    if (gMaterial.color.r == 1.0f)
     {
-         
+        uint32_t width, height;
+        gTexture.GetDimensions(width,height);
+        float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
+        output.color.rgb = float32_t3(0.0f,0.0f,0.0f);
+        output.color.a = 1.0f;
+        
+        for (int32_t x = 0; x < 3; ++x)
+        {
+            for (int32_t y = 0; y < 3; ++y)
+            {
+                float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+                float32_t3 fetchColor = gTexture.Sample(gSampler,texcoord).rgb;
+                output.color.rgb += fetchColor * kKernel3x3[x][y];
+            }
+
+        }
+        
     }
     
         return output;
